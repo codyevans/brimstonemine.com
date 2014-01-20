aa0105e73467689a48bb7d428e7afea54e8cd115
Cody Evans <codyevansdesign@gmail.com>
2014-01-20 16:23:21 -0700

    re-design home page setup

---
+++ home/index.php
@@ -0,0 +1,40 @@
+<?php 
+    // title tag
+    $title = 'Brimstone Mining, Inc.';
+?>
+<?php include '../parts/_doctype.php'; ?>
+
+    <body class="home">
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+    <?php include '../parts/_main-nav.php'; ?>
+
+     <div class="grid">
+        <div class="col-1-1">
+               
+             <div class="contained">
+                <h2>Brimstone Mining, Inc.</h2>
+                <hr>
+                <p>
+                    <strong>Brimstone Mining, Inc.</strong> (BMI) is a Montana Corporation formed in January 1996. The Company was formed specifically to acquire the Mayflower Mine and associated assets. Some of the claims and all of the equipment and supplies at the Mayflower are owned directly by BMI. The rest of the claims are owned by West Mayflower Mining Co. and Mayflower Mining Co. which are both Montana corporations formed in the early 1900’s and owned by BMI..
+                </p>
+            </div>
+
+            <div class="main-links">
+                <a href="/mayflower-mine">
+                    Mayflower Mine
+                </a>
+                 <a href="/maps/">
+                    Brimstone Maps
+                </a>
+                 <a href="/press/">
+                    Press Releases
+                </a>
+            </div>
+
+        </div> <!-- /col -->
+    </div> <!-- /grid -->
+
+<?php include '../parts/_footer.php'; ?>
